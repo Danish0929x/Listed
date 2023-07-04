8365ab4f077bbc939c8b8017bf73e416a3beb01c
Danish0929x <mouzzamdanishh@gmail.com>
2023-07-04 12:20:36 +0530

    New

---
+++ src/components/login/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import google from "../../assets/images/google.svg";
+import apple from "../../assets/images/apple.svg";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login-left">
+        <h1>Board.</h1>
+      </div>
+      <div className="login-right">
+        <div className="lr-main">
+          <h2>Sign In</h2>
+          <p>Sign in to your account</p>
+          <div className="lrm-top">
+            <div className="lrm-top-item">
+              <img src={google} />
+              <p>Sign in with Google</p>
+            </div>
+            <div className="lrm-top-item">
+              <img src={apple} />
+              <p>Sign in with Apple</p>
+            </div>
+          </div>
+          <div className="lrm-main">
+            <div className="form-group">
+              <label>Email address</label>
+              <input type="text" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="text" />
+            </div>
+            <a>Forgot password?</a>
+            <button>Sign In</button>
+          </div>
+          <p>Don't have an account? <a>Register here</a></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
